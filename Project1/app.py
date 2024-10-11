3360def156b3f8e2f4406f727972e63127d8aa5b
malama chali <chalimalama@gmail.com>
2024-10-11 13:48:21 +0100

    n

--- Project1/app.py
+++ Project1/app.py
@@ -4,11 +4,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Load your Keras model
-model_path = 'Project1/ClassificationProject/models/my_first_model.keras'  # Adjust the path if needed
+model_path = 'Project1/ClassificationProject/models/my_first_model.keras'  # Update with your relative model path if stored locally
 model = load_model(model_path)
-
-# Define class names corresponding to your model's output indices
-class_names = ["rose", "tulip", "daisy", "sunflower", "dandelion"]  # Update with actual class names
 
 # Set up Streamlit interface
 st.title("Flower Image Recognition Keras Model Prediction App")
@@ -25,11 +22,8 @@ if uploaded_file is not None:
 
     # Make prediction
     prediction = model.predict(img_array)
-    predicted_class_index = np.argmax(prediction, axis=1)[0]  # Get class index
+    predicted_class = np.argmax(prediction, axis=1)  # Get class index
 
-    # Map predicted class index to class name
-    predicted_class_name = class_names[predicted_class_index]
-
-    # Display the predicted class name and uploaded image
+    # Display results
     st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
-    st.write(f"Predicted class: {predicted_class_name}")
+    st.write(f"Predicted class: {predicted_class[0]}")
